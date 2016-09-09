7804eef36800079c57c987aa16ef9faa3c672b1d
Fabrício Godoy <fabricio@verion.ind.br>
2016-09-09 16:32:07 -0300

    Fix to deal with null, arrays and RFC3339 values

--- app/parser/controller.js
+++ app/parser/controller.js
@@ -17,11 +17,11 @@ function parserController() {
     vm.generate = generate;
     vm.closeError = closeError;
     vm.isReadOnly = true;
-    
+
     function generate() {
         closeOutput();
         closeError();
-        
+
         var parsedJson;
         try {
             parsedJson = JSON.parse(vm.jsonInput);
@@ -30,39 +30,48 @@ function parserController() {
             vm.hasError = true;
             return;
         }
-        
+
         if (!_.isArray(parsedJson)) {
             vm.errorOutput = 'The provided JSON is not an array';
             vm.hasError = true;
             return;
         }
-        
+
         var entry = _.template('\tnew <%= typeName %> {\n<%= fields %>\n\t}');
         var field = _.template('\t\t<%= name %> = <%= value %>');
         var array = _.template('<%= readonly %><%= typeName %>[] <%= arrayName %> =\n{\n<%= elements %>\n};');
-        
+        var rfc3339 = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d{3})?([+-]\d{2}):(\d{2})$/;
+
         var entries = [];
-        parsedJson.forEach(function(element) {
+        parsedJson.forEach(function (element) {
             var fields = [];
-            _.forIn(element, function(value, key) {
-                if (_.isString(value))
-                    value = '"' + value + '"';
-                
-                fields.push(field({'name' : key, 'value' : value}));
+            _.forIn(element, function (value, key) {
+                if (value === null)
+                    value = "null";
+                else if (_.isArray(value))
+                    value = 'new byte[] {' + value + '}';
+                else if (_.isString(value)) {
+                    if (rfc3339.test(value))
+                        value = 'RFC3339("' + value + '")';
+                    else
+                        value = '"' + value + '"';
+                }
+
+                fields.push(field({ 'name': key, 'value': value }));
             });
-            entries.push(entry({'typeName': vm.typeName, 'fields' : fields.join(', \n')}));
+            entries.push(entry({ 'typeName': vm.typeName, 'fields': fields.join(', \n') }));
         });
-        
+
         var readonlyValue = vm.isReadOnly ? 'readonly ' : '';
-        vm.csOutput = array({'readonly': readonlyValue, 'typeName': vm.typeName, 'arrayName': vm.arrayName, 'elements' : entries.join(', \n')});
+        vm.csOutput = array({ 'readonly': readonlyValue, 'typeName': vm.typeName, 'arrayName': vm.arrayName, 'elements': entries.join(', \n') });
         vm.hasOutput = true;
     }
-    
+
     function closeOutput() {
         vm.hasOutput = false;
         vm.csOutput = '';
     }
-    
+
     function closeError() {
         vm.hasError = false;
         vm.errorOutput = '';
